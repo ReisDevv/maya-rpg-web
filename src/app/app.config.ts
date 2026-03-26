fd56993b58cbfbca4b2dfc04cd2a28f0eb1fd7f3
Nelson Dos Reis Gomes Souza <nelsondosreisgomessouza@gmail.com>
2026-03-26 11:23:44 -0300

    chore: atualiza dependências e configurações globais de injeção

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,22 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
-
 import { routes } from './app.routes';
 import { authInterceptor } from './data/interceptors/auth.interceptor';
-import { PATIENT_REPOSITORY, AUTH_SERVICE } from './core/tokens/injection-tokens';
-import { PatientRepository } from './data/repositories/patient.repository';
+
+// Importando os Tokens e Services
+import {
+  PATIENT_REPOSITORY,
+  EXERCISE_REPOSITORY,
+  PRESCRIPTION_REPOSITORY,
+  AUTH_SERVICE,
+} from './core/tokens/injection-tokens';
 import { AuthService } from './data/services/auth.service';
+
+// Importando os Repositórios REAIS (Adeus, Mocks!)
+import { PatientRepository } from './data/repositories/patient.repository';
+import { ExerciseRepository } from './data/repositories/exercise.repository';
+import { PrescriptionRepository } from './data/repositories/prescription.repository';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -14,8 +24,10 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideHttpClient(withInterceptors([authInterceptor])),
 
-    // TODO: trocar por PatientRepository quando backend estiver pronto
+    // Injeção de Dependências Reais apontando para a sua API
     { provide: PATIENT_REPOSITORY, useClass: PatientRepository },
+    { provide: EXERCISE_REPOSITORY, useClass: ExerciseRepository },
+    { provide: PRESCRIPTION_REPOSITORY, useClass: PrescriptionRepository },
     { provide: AUTH_SERVICE, useClass: AuthService },
   ],
 };
